aae20fb34c166dfeb02fb1d6c2e0bb3e4ecd95cb
Saranganrajendran365 <saranganrajendran365@gmail.com>
2024-03-26 09:57:42 +0530

    project setupp

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,36 @@
+"use client";
+import WordMark from "@/components/WordMark";
+import { Content } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
+import Link from "next/link";
+type NavBarProps = {
+  settings: Content.SettingsDocument;
+};
+
+export default function NavBar({ settings }: NavBarProps) {
+  return (
+    <nav aria-label="Main" className="md-:py-6 px-4 py-4 md:px-6">
+      <div
+        className="mx-auto  flex max-w-6xl flex-col justify-between 
+      py-2 font-medium text-white md:flex-row md:items-center"
+      >
+        <Link href="/">
+          <WordMark />
+          <span className="sr-only">Glisten Ai Home page</span>
+        </Link>
+        <ul className="flex gap-6">
+          {settings.data.navigation.map((item) => (
+            <li key={item.label}>
+              <PrismicNextLink
+                field={item.link}
+                className="inline-flex min-h-11 items-center"
+              >
+                {item.label}
+              </PrismicNextLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
