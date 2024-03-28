2479abf33331e9ba46a9ba00312120d7effb5576
Saranganrajendran365 <saranganrajendran365@gmail.com>
2024-03-28 10:53:33 +0530

    glass scroll effect in showcase part

---
+++ src/slices/Showcase/AnimatedContent.tsx
@@ -0,0 +1,43 @@
+"use client";
+import usePrefersReducedMotion from "@/hooks/usePrefersReducedMotion";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import React, { useRef } from "react";
+
+export default function AnimatedContent({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const container = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+  gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+  useGSAP(
+    () => {
+      if (prefersReducedMotion) {
+        gsap.set(container.current, { y: 0 });
+        return;
+      }
+      gsap.fromTo(
+        container.current,
+        { y: 100 },
+        {
+          y: 0,
+          ease: "power2.inOut",
+          duration: 1,
+          scrollTrigger: {
+            trigger: container.current,
+            start: "top bottom-=40%",
+            toggleActions: "play pause resume reverse",
+            // markers: true,
+          },
+        },
+      );
+    },
+    { scope: container },
+  );
+
+  return <div ref={container}>{children}</div>;
+}
